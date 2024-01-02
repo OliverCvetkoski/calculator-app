982a75a50f0c36010fdece0f74df676919a06617
Oliver Cvetkoski <cvetbt@gmail.com>
2024-01-02 17:12:43 +0100

    added dark and light mode

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const displayText = document.querySelector(".text");
+const theme = document.querySelector(".theme");
+const img = document.querySelector(".img");
+const container = document.querySelector(".container");
+const numbers = document.querySelectorAll(".numbers");
+const controls = document.querySelectorAll(".controls");
+const display = document.querySelector(".display");
 
-function addToDisplay(num) {
-  displayText.value += num;
-}
+let isDarkMode = false;
 
-function addControls(control) {
-  displayText.value += control;
+function addToDisplay(value) {
+  displayText.value += value;
 }
 
 function clearDisplay() {
@@ -24,6 +28,47 @@ function validateInput(event) {
 }
 
 function summarize() {
-  const result = eval(displayText.value); // Using eval to evaluate the mathematical expression
+  const result = eval(displayText.value);
   displayText.value = result;
 }
+
+function darkMode() {
+  container.style.backgroundColor = "black";
+  img.src = "assets/Moon.svg";
+  theme.innerText = "Dark Mode";
+  theme.style.color = "white";
+  numbers.forEach((btn) => {
+    btn.classList.remove("numbersLight");
+  });
+  controls.forEach((btn) => {
+    btn.classList.remove("controlsLight");
+  });
+  display.classList.remove("displayLight");
+  displayText.classList.remove("textLight");
+}
+
+function lightMode() {
+  numbers.forEach((btn) => {
+    btn.classList.add("numbersLight");
+  });
+  container.style.backgroundColor = "white";
+  img.src = "assets/Sun.svg";
+  theme.innerText = "Light Mode";
+  theme.style.color = "black";
+  controls.forEach((btn) => {
+    btn.classList.add("controlsLight");
+  });
+  display.classList.add("displayLight");
+  displayText.classList.add("textLight");
+}
+
+function toggleTheme() {
+  isDarkMode = !isDarkMode;
+  if (isDarkMode) {
+    darkMode();
+  } else {
+    lightMode();
+  }
+}
+
+toggleTheme();
